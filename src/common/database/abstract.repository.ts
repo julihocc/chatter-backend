a6ea3db323891cc3e3e9cfeb85c5f9e6c4c1b9ea
julihocc <jcc30986@gmail.com>
2024-10-10 17:35:16 -0600

    Refactor AbstractRepository to include findOneAndUpdate method

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -1,6 +1,6 @@
 import { Logger, NotFoundException } from '@nestjs/common';
 import { AbstractDocument } from './abstract.schema';
-import { FilterQuery, Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   protected abstract readonly logger: Logger;
@@ -19,7 +19,25 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     const document = await this.model.findOne(filterQuery).lean<TDocument>();
 
     if (!document) {
-      this.logger.warn('Document was not found with filterQuery');
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<TDocument>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
       throw new NotFoundException('Document not found');
     }
 
