e29303cf975d6285417aa648a56610d6c4197c93
julihocc <jcc30986@gmail.com>
2024-10-10 17:12:46 -0600

    Add AbstractRepository class for common database functionality

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+import { Model, Types } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+}
