f5ee9fe2f434ab967c78a8f1601af48229dc18f9
julihocc <jcc30986@gmail.com>
2024-10-11 11:50:36 -0600

    Refactor DatabaseModule to include forFeature method

--- dist/common/database/database.module.js
+++ dist/common/database/database.module.js
@@ -11,6 +11,9 @@ const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const mongoose_1 = require("@nestjs/mongoose");
 let DatabaseModule = class DatabaseModule {
+    static forFeature(models) {
+        return mongoose_1.MongooseModule.forFeature(models);
+    }
 };
 exports.DatabaseModule = DatabaseModule;
 exports.DatabaseModule = DatabaseModule = __decorate([
