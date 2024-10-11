8b12155238027a6b144423ed460a17c32dfcc684
julihocc <jcc30986@gmail.com>
2024-10-11 12:41:45 -0600

    Refactor findOne method in AbstractRepository to handle not found documents

--- src/common/database/abstract.repository.ts
+++ src/common/database/abstract.repository.ts
@@ -16,7 +16,7 @@ export abstract class AbstractRepository<T extends AbstractEntity> {
   }
 
   async findOne(filterQuery: FilterQuery<T>): Promise<T> {
-    const document = await this.model.findOne(filterQuery).lean<T>();
+    const document = await this.model.findOne(filterQuery, {}).lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -38,7 +38,7 @@ export abstract class AbstractRepository<T extends AbstractEntity> {
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
-      throw new NotFoundException('Document not found');
+      throw new NotFoundException('Document not found.');
     }
 
     return document;
