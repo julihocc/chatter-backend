77af7c8ad624de95da7c9672a324c4335c60be29
julihocc <jcc30986@gmail.com>
2024-10-14 16:37:26 -0600

    Add CORS support and configure CORS options

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://127.0.0.1:5173', // Update this to match your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
