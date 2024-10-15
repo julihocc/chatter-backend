a17e29b9a5445e1b73ee0cc76e560257c43db59e
julihocc <jcc30986@gmail.com>
2024-10-15 15:46:12 -0600

    Add cookie-parser middleware for parsing cookies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -19,6 +20,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
 }
