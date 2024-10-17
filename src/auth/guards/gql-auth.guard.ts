1ea4bb6800d50cc2e04b7220912da84f30e60d9d
julihocc <jcc30986@gmail.com>
2024-10-16 18:57:47 -0600

    Refactor GqlAuthGuard to remove unnecessary generic type in getRequest method

--- src/auth/guards/gql-auth.guard.ts
+++ src/auth/guards/gql-auth.guard.ts
@@ -3,7 +3,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 export class GqlAuthGuard extends AuthGuard('jwt') {
-  getRequest<T = any>(context: ExecutionContext) {
+  getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
   }
